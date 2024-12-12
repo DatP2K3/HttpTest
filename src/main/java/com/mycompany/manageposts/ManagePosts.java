9b51410ab31a1c95b2200a9f4ad4e1ebd0751a3d
Đạt Phan Coder <macbookairm3@MacBook-Air-cua-Macbook.local>
2024-12-12 08:53:57 +0700

    Start Project

---
+++ src/main/java/com/mycompany/manageposts/ManagePosts.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.manageposts;
+
+/**
+ *
+ * @author macbookairm3
+ */
+public class ManagePosts {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
